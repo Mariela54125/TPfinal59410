88ca38a2d01a5080d454b5a3fcbc67103530aae0
Mariela54125 <163675360+Mariela54125@users.noreply.github.com>
2024-11-25 22:47:52 -0300

    Update 3_Objetos_procedure.sql

--- 3_Objetos_procedure.sql
+++ 3_Objetos_procedure.sql
@@ -1,31 +1,67 @@
-
-#TOTAL DE VENTAS POR CLIENTES: EJEMPLO CLIENTE 1#
+# TOTAL DE VENTAS POR CLIENTES: EJEMPLO CLIENTE 1 #
 DELIMITER //
 
-CREATE PROCEDURE TotalVentasCliente(IN cliente_id INT) 
-BEGIN     
-    SELECT SUM(total) AS total_ventas     
-    FROM VENTA     
-    WHERE id_cliente = cliente_id; 
+CREATE PROCEDURE TotalVentasCliente(IN cliente_id INT)
+BEGIN
+    -- Verificar si el cliente existe
+    DECLARE cliente_existente INT;
+
+    SELECT COUNT(*) INTO cliente_existente
+    FROM CLIENTE
+    WHERE id_cliente = cliente_id;
+
+    -- Si el cliente no existe, devolver mensaje
+    IF cliente_existente = 0 THEN
+        SELECT 'El cliente no existe' AS mensaje_error;
+    ELSE
+        -- Realizar la consulta de ventas
+        SELECT 
+            C.nombre AS cliente_nombre,
+            SUM(V.total) AS total_ventas
+        FROM 
+            VENTA V
+            INNER JOIN CLIENTE C ON V.id_cliente = C.id_cliente
+        WHERE 
+            V.id_cliente = cliente_id
+        GROUP BY 
+            C.nombre;
+    END IF;
 END //
 
 DELIMITER ;
 
-CALL TotalVentasCliente(1); #CAMBIAR POR ID DE CLIENTE QUE DESEA CONSULTAR#
+CALL TotalVentasCliente(2); # CAMBIAR POR ID DE CLIENTE QUE DESEA CONSULTAR #
 
-
-
-#TOTAL DE VENTAS POR VENDEDOR#
+# TOTAL DE VENTAS POR VENDEDOR #
 DELIMITER //
 
 CREATE PROCEDURE TotalVentasPorVendedor(IN vendedor_id INT)
 BEGIN
-    SELECT SUM(total) AS total_ventas
-    FROM VENTA
-    WHERE id_vendedor = vendedor_id; # Filtra por el ID del vendedor#
+    -- Verificar si el vendedor existe
+    DECLARE vendedor_existente INT;
+
+    SELECT COUNT(*) INTO vendedor_existente
+    FROM VENDEDOR
+    WHERE id_vendedor = vendedor_id;
+
+    -- Si el vendedor no existe, devolver mensaje
+    IF vendedor_existente = 0 THEN
+        SELECT 'El vendedor no existe' AS mensaje_error;
+    ELSE
+        -- Realizar la consulta de ventas
+        SELECT 
+            V.nombre AS vendedor_nombre,
+            SUM(VT.total) AS total_ventas
+        FROM 
+            VENTA VT
+            INNER JOIN VENDEDOR V ON VT.id_vendedor = V.id_vendedor
+        WHERE 
+            VT.id_vendedor = vendedor_id
+        GROUP BY 
+            V.nombre;
+    END IF;
 END //
 
 DELIMITER ;
 
-
-CALL TotalVentasPorVendedor(3); #cambiar por id_vendedor que desea informacion#
+CALL TotalVentasPorVendedor(3); # CAMBIAR POR ID DE VENDEDOR QUE DESEA INFORMACION #
